6d25574288e2400200f8bb325b99245f50124839
Ezra Fast <ezrafast@tutanota.com>
2024-07-22 17:43:35 -0500

    Enumeration commands

---
+++ ActiveDirectory/EnumerationCommands.ps1
@@ -0,0 +1,13 @@
+# Creating a map of an Active Directory domain:
+#
+# Users
+# Groups
+# Domain Information
+# Domain joined computers
+# Domain joined operating systems
+# machines you can access as Local Administrator
+# Current logon sessions on the domain
+# SPNs (services integrated with AD)
+# Enumerate for GenericAll ActiveDirectoryRights permissions 
+# Enumerate domain shares
+#
